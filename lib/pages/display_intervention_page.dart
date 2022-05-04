88ae4254b203243cd50fe62f5e750fa71f8f428b
Leloup <alexleloup@yahoo.fr>
2022-05-04 17:44:32 +0200

    Add resizeToAvoidBottomInset attribute to listview

--- lib/pages/display_intervention_page.dart
+++ lib/pages/display_intervention_page.dart
@@ -15,6 +15,8 @@ class DisplayIntervention extends StatefulWidget {
 class _DisplayInterventionState extends State<DisplayIntervention> {
   final int paneProportion = 30;
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -27,213 +29,207 @@ class _DisplayInterventionState extends State<DisplayIntervention> {
       children: [
         Flexible(
           flex: paneProportion,
-          child: Pane1(),
+          child: Container(
+              color: Colors.white,
+              child: Scaffold(
+                  resizeToAvoidBottomInset: true,
+                  body: ListView(
+                    padding: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).viewInsets.bottom),
+                    children: <Widget>[
+                      GestureDetector(
+                          onTap: () {
+                            print("Click : Header");
+                          },
+                          child: const Card(
+                            child: ListTile(
+                                title: Text("Feu Capgemini Rennes"),
+                                subtitle: Text(
+                                    "7 Rue Claude Chappe, 35510 Cesson-Sévigné"),
+                                trailing: Text("2:32")),
+                          )),
+                      GestureDetector(
+                          onTap: () {
+                            print("Click : Tableau des moyens");
+                          },
+                          child: const Card(
+                              child: ListTile(
+                                  title: Text("Tableau des moyens"),
+                                  trailing: Icon(
+                                      Icons.arrow_circle_right_outlined)))),
+                      GestureDetector(
+                          onTap: () {
+                            print("Click : Drone view");
+                          },
+                          child: const Card(
+                              child: ListTile(
+                                  title: Text("Vue drone"),
+                                  trailing: Icon(
+                                      Icons.arrow_circle_right_outlined)))),
+                      GestureDetector(
+                          onTap: () {
+                            print("Click : Interventions list");
+                          },
+                          child: const Card(
+                              child: ListTile(
+                                  title: Text("Liste des interventions"),
+                                  trailing: Icon(
+                                      Icons.arrow_circle_right_outlined)))),
+                      //SizedBox(height: 1, child: DecoratedBox(decoration: BoxDecoration(color: Colors.black))),
+                      GestureDetector(
+                          onTap: () {
+                            print("Click : Draw");
+                          },
+                          child: const Card(
+                            child: ListTile(
+                                title: Text("Dessiner"),
+                                trailing: Icon(Icons.border_color_outlined)),
+                            margin: EdgeInsets.only(bottom: 5),
+                          )),
+                      //SizedBox(height: 1, child: DecoratedBox(decoration: BoxDecoration(color: Colors.black))),
+                      GestureDetector(
+                          onTap: () {
+                            print("Click : Retour");
+                          },
+                          child: Card(
+                            child: Form(
+                              key: _formKey,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    // The validator receives the text that the user has entered.
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 16.0),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        // Validate returns true if the form is valid, or false otherwise.
+                                        if (_formKey.currentState!.validate()) {
+                                          // If the form is valid, display a snackbar. In the real world,
+                                          // you'd often call a server or save the information in a database.
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            const SnackBar(
+                                                content:
+                                                    Text('Processing Data')),
+                                          );
+                                        }
+                                      },
+                                      child: const Text('Submit'),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )),
+                    ],
+                  ))),
         ),
         Flexible(
           flex: 100 - paneProportion,
-          child: MapWidget(),
-        ),
-      ],
-    );
-  }
-}
-
-class Pane1 extends StatelessWidget {
-
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        child: ListView(
-          children: <Widget>[
-            GestureDetector(
-                onTap: () {
-                  print("Click : Header");
-                },
-                child: const Card(
-                  child: ListTile(
-                      title: Text("Feu Capgemini Rennes"),
-                      subtitle:
-                          Text("7 Rue Claude Chappe, 35510 Cesson-Sévigné"),
-                      trailing: Text("2:32")),
-                )),
-            GestureDetector(
-                onTap: () {
-                  print("Click : Tableau des moyens");
-                },
-                child: const Card(
-                    child: ListTile(
-                        title: Text("Tableau des moyens"),
-                        trailing: Icon(Icons.arrow_circle_right_outlined)))),
-            GestureDetector(
-                onTap: () {
-                  print("Click : Drone view");
-                },
-                child: const Card(
-                    child: ListTile(
-                        title: Text("Vue drone"),
-                        trailing: Icon(Icons.arrow_circle_right_outlined)))),
-            GestureDetector(
-                onTap: () {
-                  print("Click : Interventions list");
-                },
-                child: const Card(
-                    child: ListTile(
-                        title: Text("Liste des interventions"),
-                        trailing: Icon(Icons.arrow_circle_right_outlined)))),
-            //SizedBox(height: 1, child: DecoratedBox(decoration: BoxDecoration(color: Colors.black))),
-            GestureDetector(
-                onTap: () {
-                  print("Click : Draw");
-                },
-                child: const Card(
-                  child: ListTile(
-                      title: Text("Dessiner"),
-                      trailing: Icon(Icons.border_color_outlined)),
-                  margin: EdgeInsets.only(bottom: 5),
-                )),
-            //SizedBox(height: 1, child: DecoratedBox(decoration: BoxDecoration(color: Colors.black))),
-            GestureDetector(
-                onTap: () {
-                  print("Click : Retour");
-                },
-                child: Card(
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 16.0),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              // Validate returns true if the form is valid, or false otherwise.
-                              if (_formKey.currentState!.validate()) {
-                                // If the form is valid, display a snackbar. In the real world,
-                                // you'd often call a server or save the information in a database.
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(content: Text('Processing Data')),
-                                );
-                              }
-                            },
-                            child: const Text('Submit'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                )),
-          ],
-        ));
-  }
-}
-
-class MapWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(48.117266, -1.6777926),
-        zoom: 10,
-      ),
-      children: <Widget>[
-        TileLayerWidget(
-          options: TileLayerOptions(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c'],
+          child: FlutterMap(
+            options: MapOptions(
+              center: LatLng(48.117266, -1.6777926),
+              zoom: 10,
+            ),
+            children: <Widget>[
+              TileLayerWidget(
+                options: TileLayerOptions(
+                  urlTemplate:
+                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'],
+                ),
+              ),
+            ],
           ),
         ),
       ],
