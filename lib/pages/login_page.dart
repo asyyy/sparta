afa5be665c52b1a439ba629d3704268e2ff8cc1c
Sophie Gadrey <gadreysophie@gmail.com>
2022-05-04 16:09:36 +0200

    implemented the login page version 1 and added responsive dependencies.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive/responsive.dart';
+
+import 'display_intervention_page.dart';
 
 ///
 /// Widget that allows login
@@ -11,6 +14,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
+
   @override
   void initState() {
     super.initState();
@@ -19,23 +23,57 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child : Row(
+        child : ResponsiveRow(
           children: [
-            Column (
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                 Image.asset(
-
-                    'images/login.png',
+            Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: const BoxDecoration(
+                     image:DecorationImage(
+                         image: AssetImage ("images/login.png"),
+                         fit: BoxFit.cover),
                   ),
-                  Text ("loginpage"),
-                ]
-            ),
-            Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const Text ("loginpage"),
-                ]
+                  child : Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children:  <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.4,
+                          height: MediaQuery.of(context).size.height*0.4,
+                          color: Colors.white,
+                          child: Column(
+                            children: [
+                              const Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        labelText: 'User Name',
+                                        hintText: 'Enter your user name'
+                                    ),
+                                  ),
+                                ),
+                              const Padding(
+                                padding: EdgeInsets.all(10),
+                                child: TextField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Password',
+                                      hintText: 'Enter your secure password'
+                                  ),
+                                ),
+                              ),
+                             TextButton(
+                               onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) =>  const DisplayIntervention())),
+                               child: const Text("Login", style: TextStyle(fontWeight: FontWeight.bold),),)
+                            ],
+                          ),
+                        ),
+                      ]
+                  ),
+
             ),
           ],
         )
