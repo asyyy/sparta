7f47a0493977159948d149f9cd69f9a3ce300881
Sophie Gadrey <gadreysophie@gmail.com>
2022-05-05 10:08:05 +0200

    added a form for validation of textformfield and modified the picture login.png to erase white border.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive/responsive.dart';
 
@@ -13,7 +14,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -23,61 +24,95 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child : ResponsiveRow(
-          children: [
-            Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  decoration: const BoxDecoration(
-                     image:DecorationImage(
-                         image: AssetImage ("images/login.png"),
-                         fit: BoxFit.cover),
-                  ),
-                  child : Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children:  <Widget>[
-                        Container(
-                          width: MediaQuery.of(context).size.width*0.4,
-                          height: MediaQuery.of(context).size.height*0.4,
-                          color: Colors.white,
-                          child: Column(
-                            children: [
-                              const Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: TextField(
-                                    decoration: InputDecoration(
-                                        border: OutlineInputBorder(),
-                                        labelText: 'User Name',
-                                        hintText: 'Enter your user name'
-                                    ),
-                                  ),
-                                ),
-                              const Padding(
-                                padding: EdgeInsets.all(10),
-                                child: TextField(
-                                  obscureText: true,
-                                  decoration: InputDecoration(
-                                      border: OutlineInputBorder(),
-                                      labelText: 'Password',
-                                      hintText: 'Enter your secure password'
-                                  ),
-                                ),
-                              ),
-                             TextButton(
-                               onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) =>  const DisplayIntervention())),
-                               child: const Text("Login", style: TextStyle(fontWeight: FontWeight.bold),),)
-                            ],
-                          ),
+        child: ResponsiveRow(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/login.png"), fit: BoxFit.cover),
+          ),
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Form(
+                  key: _formKey,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(1),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset:
+                              const Offset(0, 3), // changes position of shadow
                         ),
-                      ]
+                      ],
+                    ),
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: MediaQuery.of(context).size.height * 0.35,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'User Name',
+                              hintText: 'Enter your user name'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "TEXT_REQUIRED";
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                              hintText: 'Enter your secure password'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "TEXT_REQUIRED";
+                            }
+                            return null;
+                          },
+                        ),
+                        ElevatedButton(
+                          onPressed: () => {
+                            if (_formKey.currentState!.validate())
+                              {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (_) =>
+                                            const DisplayIntervention()))
+                                //ScaffoldMessenger.of(context).showSnackBar (const SnackBar(content: Text('Processing Data')),)
+                              }
+                            else
+                              {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Processing Data')),
+                                )
+                              }
+                          },
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-
-            ),
-          ],
-        )
-    );
+                ),
+              ]),
+        ),
+      ],
+    ));
   }
-
 }
