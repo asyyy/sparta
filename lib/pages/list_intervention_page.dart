c880501ac1987756cf1560dbb3672b0939569d6f
Sophie Gadrey <gadreysophie@gmail.com>
2022-05-06 10:24:14 +0200

    delete display_intervention_page.dart and create the list_intervention_page.dart file

--- lib/pages/list_intervention_page.dart
+++ lib/pages/list_intervention_page.dart
@@ -2,6 +2,10 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:positioned_tap_detector_2/positioned_tap_detector_2.dart';
+
 import 'package:projet_groupe_c/pages/loading_page.dart';
 import 'package:projet_groupe_c/pages/login_page.dart';
 import 'package:projet_groupe_c/pages/new_intervention_page.dart';
@@ -9,14 +13,15 @@ import 'package:projet_groupe_c/pages/new_intervention_page.dart';
 ///
 /// Widget that allows display intervention
 ///
-class DisplayIntervention extends StatefulWidget {
-  const DisplayIntervention({Key? key}) : super(key: key);
+class ListIntervention extends StatefulWidget {
+  const ListIntervention({Key? key}) : super(key: key);
   @override
-  _DisplayInterventionState createState() => _DisplayInterventionState();
+  _ListInterventionState createState() => _ListInterventionState();
 }
 
-class _DisplayInterventionState extends State<DisplayIntervention> {
+class _ListInterventionState extends State<ListIntervention> {
   late AppBar appBar;
+  late final MapController mapController ;
   @override
   void initState() {
     super.initState();
@@ -46,6 +51,7 @@ class _DisplayInterventionState extends State<DisplayIntervention> {
         )
       ],
     );
+    mapController = MapController();
   }
 
   @override
@@ -57,7 +63,7 @@ class _DisplayInterventionState extends State<DisplayIntervention> {
           SingleChildScrollView(
             scrollDirection: Axis.vertical,
             child:
-                Column(
+            Column(
               children: [
                 Container(
                   width: MediaQuery.of(context).size.width * 0.5,
@@ -86,8 +92,23 @@ class _DisplayInterventionState extends State<DisplayIntervention> {
             Row(
               children: [
                 Container(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text("hello"),
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    height: MediaQuery.of(context).size.height-appBar.preferredSize.height-75,
+                    child: FlutterMap(
+                      options: MapOptions(
+                        center: LatLng(48.117266, -1.6777926),
+                        zoom: 10,
+                      ),
+                      children: <Widget>[
+                        TileLayerWidget(
+                          options: TileLayerOptions(
+                            urlTemplate:
+                            'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                            subdomains: ['a', 'b', 'c'],
+                          ),
+                        ),
+                      ],
+                    )
                 ),
               ],
             ),
@@ -97,12 +118,12 @@ class _DisplayInterventionState extends State<DisplayIntervention> {
                   width: MediaQuery.of(context).size.width * 0.5,
                   child: ElevatedButton(
                     onPressed: () => {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (_) =>
-                                        const LoadingPage()))
-                        },
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) =>
+                              const LoadingPage()))
+                    },
                     child: const Text(
                       "Rejoindre l'intervention",
                       style: TextStyle(fontWeight: FontWeight.bold),
