0ca2082372a3e116b1f5bbb0064478e72d6d526c
Guillaume Le Dourner <guillaume.le-dourner@etudiant.univ-rennes1.fr>
2022-05-09 16:42:05 +0200

    Page redirection from intervention list to intervention page

--- lib/pages/list_intervention_page.dart
+++ lib/pages/list_intervention_page.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 import 'dart:ffi';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
