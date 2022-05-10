1d92c439aa26db37f099bd6873e9da82a52c66f6
Alexys Guerin <alexys.guerin@dolmen-tech.com>
2022-05-10 09:38:40 +0200

    remove useless comment, keep print if someone need to see it does

--- lib/model/Mapper.dart
+++ lib/model/Mapper.dart
@@ -9,22 +9,6 @@ class Mapper {
   Map<String, String> vehiclesTypes = {};
   Map<String, String> sinisterTypes = {};
 
-  // init() {
-  //   print("init");
-  //   List<dynamic> list = List.empty();
-
-  //   MapperService.getSinisterType().then((value) => {
-  //         for (var object in json.decode(value.body))
-  //           {
-  //             print(object['_id']),
-  //             sinisterTypes.putIfAbsent(object['_id'], () => object['libelle'])
-  //           }
-  //       });
-  //   print("SinisterMap size is " + sinisterTypes.length.toString());
-  //   sinisterTypes.forEach((key, value) {
-  //     print('key is ' + key);
-  //   });
-  // }
   fillMapper(dynamic value) {
     for (var object in json.decode(value.body)) {
       sinisterTypes.putIfAbsent(object['libelle'], () => object['_id']);
@@ -41,7 +25,5 @@ class Mapper {
         .then((value) => {print("ValidationVehicles"), fillMapper(value)});
     MapperService.getSinisterType()
         .then((value) => {print("SinisterType"), fillMapper(value)});
-    // MapperService.getValidationsVehicles().then((value) => print(value.body));
-    // MapperService.getVehiclesType().then((value) => print(value.body));
   }
 }
